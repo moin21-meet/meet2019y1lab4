fe9181818771d5d22bc68235316e5d463a8c8f18
moin21-meet <moin21@meet.mit.edu>
2019-07-24 09:57:55 -0400

    Stopping here

--- onekeypress.py
+++ onekeypress.py
@@ -2,9 +2,26 @@ import turtle
 a=turtle.Turtle()
 a.goto(0,0)
 def up():
-     fd(50)
-     lt(50)
+     a.forward(50)
+def back():
+     a.back(50)
+def left():
+     a.left(90)
+def right():
+     a.right(90)
+def color_green():
+     a.color('green')
+def color_blue():
+     a.color('blue')
+def color_red():
+     a.color('red')
 
-a.onkeypress(up,'w')
-a.listen()
-a.mainloop()
+turtle.onkeypress(up, 'w')
+turtle.onkeypress(back, 's')
+turtle.onkeypress(left, "a")
+turtle.onkeypress(right, 'd')
+turtle.onkeypress(color_green, 'g')
+turtle.onkeypress(color_red, 'r')
+turtle.onkeypress(color_blue, 'b')
+turtle.listen()
+turtle.mainloop()
